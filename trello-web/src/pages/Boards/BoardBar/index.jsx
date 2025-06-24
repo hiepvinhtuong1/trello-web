ede4e5472a339d148190c8eaa7feb19e3ab00f57
hiep2004 <buituanhiepvinhtuong@gmail.com>
2025-06-24 16:49:12 +0700

    update

--- trello-web/src/pages/Boards/BoardBar/index.jsx
+++ trello-web/src/pages/Boards/BoardBar/index.jsx
@@ -11,13 +11,13 @@ import Tooltip from "@mui/material/Tooltip";
 import Button from "@mui/material/Button";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 const MENU_STYLES = {
-	color: "primary.main",
-	backgroundColor: "white",
+	color: "white",
+	backgroundColor: "transparent",
 	border: "none",
 	padding: "5px",
 	borderRadius: "4px",
-	"& .MuiSvgIcon-root": {
-		color: "primary.main",
+	".MuiSvgIcon-root": {
+		color: "white",
 	},
 	"&:hover": {
 		backgroundColor: "primary.50",
@@ -37,7 +37,9 @@ function BoardBar() {
 					justifyContent: "space-between",
 					gap: 2,
 					overflowX: "auto",
-					borderTop: "1px solid #00bfa5",
+					borderBottom: "1px solid white",
+					bgcolor: (theme) =>
+						theme.palette.mode === "dark" ? "#34495e" : "#1976d2",
 				}}
 			>
 				<Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
@@ -74,16 +76,27 @@ function BoardBar() {
 					/>
 				</Box>
 				<Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-					<Button variant="outlined" startIcon={<PersonAddIcon />}>
+					<Button
+						startIcon={<PersonAddIcon />}
+						sx={{
+							color: "white",
+							borderColor: "white",
+							"&:hover": {
+								borderColor: "white",
+							},
+						}}
+					>
 						Invite
 					</Button>
 					<AvatarGroup
 						max={4}
 						sx={{
+							gap: "10px",
 							"& .MuiAvatar-root": {
 								width: 34,
 								height: 34,
 								fontSize: "0.875rem",
+								border: "none",
 							},
 						}}
 					>
--- trello-web/src/theme.js
+++ trello-web/src/theme.js
@@ -1,4 +1,3 @@
-import { cyan, deepOrange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 const APP_BAR_HEIGHT = '58px'
@@ -6,7 +5,6 @@ const BOARD_BAR_HEIGHT = '60px'
 const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
 const COLUMN_HEADER_HEIGHT = '50px'
 const COLUMN_FOOTER_HEIGHT = '56px'
-// create a theme instance
 const theme = extendTheme({
     trello: {
         appBarHeight: APP_BAR_HEIGHT,
@@ -16,18 +14,8 @@ const theme = extendTheme({
         columnFooterHeight: COLUMN_FOOTER_HEIGHT
     },
     colorSchemes: {
-        light: {
-            palette: {
-                primary: teal,
-                secondary: deepOrange
-            }
-        },
-        dark: {
-            palette: {
-                primary: cyan,
-                secondary: deepOrange
-            }
-        }
+        light: {},
+        dark: {}
     },
     components: {
         MuiCssBaseline: {
@@ -38,51 +26,45 @@ const theme = extendTheme({
                         height: '8px',
                     },
                     '*::-webkit-scrollbar-thumb': {
-                        backgroundColor: '#bdc3c7',
+                        backgroundColor: '#dcdde1',
                         borderRadius: '4px',
                     },
                     '*::-webkit-scrollbar-thumb:hover': {
-                        backgroundColor: '#00b894',
+                        backgroundColor: 'white',
                         borderRadius: '4px',
                     }
                 }
             }
         },
-        // Name of the component
         MuiButton: {
             styleOverrides: {
-                // Name of the slot
                 root: {
-                    // Some CSS
                     textTransform: 'none',
+                    borderWidth: '0.5px',
                 },
             },
         },
         MuiInputLabel: {
-
             styleOverrides: {
-                root: ({ theme }) => ({
-                    color: theme.palette.primary.main,
+                root: {
                     fontSize: '0.875rem',
-                })
+                }
             },
         },
         MuiOutlinedInput: {
             styleOverrides: {
-                // Name of the slot
-                root: ({ theme }) => ({
-                    color: theme.palette.primary.main,
+                root: {
                     fontSize: '0.875rem',
-                    '& .MuiOutlinedInput-notchedOutline': {
-                        borderColor: theme.palette.primary.light,
-                    },
-                    '&:hover .MuiOutlinedInput-notchedOutline': {
-                        borderColor: theme.palette.primary.main,
-                    },
                     '& fieldset': {
+                        borderWidth: "0.5px !important",
+                    },
+                    '&:hover fieldset': {
                         borderWidth: "1px !important",
                     },
-                })
+                    '&:Mui-focused fieldset': {
+                        borderWidth: "1px !important",
+                    },
+                }
             }
         },
 
