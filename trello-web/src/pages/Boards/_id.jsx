4e21c56295c3b6128da1784459ccea7e8e54993b
hiep2004 <buituanhiepvinhtuong@gmail.com>
2025-08-06 19:05:40 +0700

    axios-call-api

--- trello-web/src/pages/Boards/_id.jsx
+++ trello-web/src/pages/Boards/_id.jsx
@@ -2,13 +2,21 @@ import Container from "@mui/material/Container";
 import AppBar from "~/components/AppBar/AppBar";
 import BoardBar from "./BoardBar/BoardBar";
 import BoardContent from "./BoardContent/BoardContent";
-import { mockData } from "~/apis/mock-data.js";
+import { fetchBoardDetailsAPI } from "~/apis";
+import { useEffect, useState } from "react";
 function Board() {
+	const [board, setBoard] = useState(null);
+	useEffect(() => {
+		const boardId = "68931465acb5b0cbd3d02415";
+		fetchBoardDetailsAPI(boardId).then((board) => {
+			setBoard(board);
+		});
+	}, []);
 	return (
 		<Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
 			<AppBar />
-			<BoardBar board={mockData?.board} />
-			<BoardContent board={mockData?.board} />
+			<BoardBar board={board} />
+			<BoardContent board={board} />
 		</Container>
 	);
 }
---
+++ trello-web/src/utils/constant.js
@@ -0,0 +1,1 @@
+export const APP_ROOT = 'http://localhost:8017';
